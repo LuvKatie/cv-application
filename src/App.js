64a19ba584325075e7947dcc0385233df5ab9372
Tony W <bobacrypto2@gmail.com>
2023-04-12 22:11:54 -0700

    Inject Education, General, and Practical components into our App components render function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './App.css';
+import { General } from './components/General';
+import { Education } from './components/Education';
+import { Practical } from './components/Practical';
 
 class App extends React.Component {
-  
+    render() {
+        return (
+            <div>
+                <General />
+                <Education />
+                <Practical />
+            </div>
+        )
+    }
 }
 
 export default App;
