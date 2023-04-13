29bceb91129706a976489a08e08104cca3e187da
Tony W <bobacrypto2@gmail.com>
2023-04-12 22:08:55 -0700

    Create Practical component which r enders a form with inputs including company name, position title, and main tasks description

---
+++ src/components/Practical.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export class Practical extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            company: {
+                text: '',
+                id: ''
+            },
+
+            position: {
+                text: '',
+                id: ''
+            },
+
+            skills: {
+                text: '',
+                id: ''
+            }
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="companyInput">Company: </label>
+                    <input type="text" id="companyInput"></input>
+                    
+                    <label htmlFor="positionInput">Poisition Title: </label>
+                    <input type="text" id="positionInput"></input>
+
+                    <label htmlFor="positionInput">Main Tasks: </label>
+                    <input type="textarea" id="positionInput"></input>
+                </form>
+            </div>
+        )
+    }
+}
