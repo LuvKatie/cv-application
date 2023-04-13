6415148b46045bb847be6370f0611f7bbfa1f60f
Tony W <bobacrypto2@gmail.com>
2023-04-12 21:56:54 -0700

    Create General component which renders a form with name, email, and phone number inputs

---
+++ src/components/General.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export class General extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            name: {
+                text: '',
+                id: ''
+            },
+
+            email: {
+                text: '',
+                id: ''
+            },
+
+            phone: {
+                text: '',
+                id: ''
+            }
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="nameInput">Name: </label>
+                    <input type="text" id="nameInput"></input>
+
+                    <label htmlFor="emailInput">Email: </label>
+                    <input type="email" id="emailInput"></input>
+
+                    <label htmlFor="phoneInput">Phone #: </label>
+                    <input type="tel" id="phoneInput"></input>
+                </form>
+            </div>
+            
+        )
+    }
+}
